6e7a78bbf35b7e3562bffcd975bd4bd5e9e8f9e6
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-07 23:46:38 +0200

    Add image to profile

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home_detail_view(request):
-    context = {}
+    context = {'user': request.user}
     return render(request, 'home/home-detail.html', context)
 
