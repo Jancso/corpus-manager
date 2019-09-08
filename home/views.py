40f74167bbc09b7fbc9f09f23f9b99f675e72a1b
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-08 15:13:29 +0200

    login page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home_detail_view(request):
     context = {'user': request.user}
     return render(request, 'home/home-detail.html', context)
