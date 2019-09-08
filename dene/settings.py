7ebdc391db03c51e80dd27f322403817c44e50e2
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-08 17:53:26 +0200

    Create user app

--- dene/settings.py
+++ dene/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home',
     'registration',
-    
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- home/models.py
+++ home/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
-class UserProfile(models.Model):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile-images/', default='user-placeholder.jpg')
-    #function = models.CharField(choices=['Glosser', 'Programmer', 'Manager'])
-
