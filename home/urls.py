34fd17bb5a177984501bf8e3d7498cb5458b7b77
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-08 21:08:04 +0200

    Link home and settings

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_detail_view
+from .views import home_detail_view, settings_detail_view
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', home_detail_view, name='home-detail')
+    path('', home_detail_view, name='home-detail'),
+    path('settings/', settings_detail_view, name='settings-detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
@@ -7,3 +7,7 @@ def home_detail_view(request):
     context = {'user': request.user}
     return render(request, 'home/home-detail.html', context)
 
+
+@login_required
+def settings_detail_view(request):
+    return redirect('users:user-update', pk=request.user.userprofile.pk)
