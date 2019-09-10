69327fe1c9b6f65e316c1dd8627974ac11fbfe6b
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-10 13:04:07 +0200

    rec tasks overview

--- workflow/views.py
+++ workflow/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render, redirect
 from .models import Recording, Task
 from .forms import RecordingForm
+from collections import namedtuple
 
 
 def workflow_view(request):
+    Rec = namedtuple(
+        'Rec',
+        ['id', 'name', 'segmentation', 'transcription', 'glossing'])
+
+    context = {
+        'recordings': []
+    }
+
     recordings = Recording.objects.all()
 
     for recording in recordings:
         tasks = Task.objects.filter(recording__name=recording.name)
-        segmentation = tasks.filter(name=Task.SEGMENTATION).get()
-        transcription = tasks.filter(name=Task.TRANSCRIPTION).get()
-        glossing = tasks.filter(name=Task.GLOSSING).get()
+        segmentation = tasks.filter(
+            name=Task.SEGMENTATION).get().get_status_display()
+        transcription = tasks.filter(
+            name=Task.TRANSCRIPTION).get().get_status_display()
+        glossing = tasks.filter(
+            name=Task.GLOSSING).get().get_status_display()
 
-        recording.segmentation = segmentation.get_status_display()
-        recording.transcription = transcription.get_status_display()
-        recording.glossing = glossing.get_status_display()
+        rec = Rec(recording.pk,
+                  recording.name,
+                  segmentation,
+                  transcription,
+                  glossing)
 
-    context = {
-        'recordings': recordings
-    }
+        context['recordings'].append(rec)
+
     return render(request, 'workflow/work_flow_view.html', context)
 
 
