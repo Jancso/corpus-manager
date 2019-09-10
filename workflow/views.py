fa2d2b73cceb5e5917ea6305dbc2b88431040035
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-10 10:58:28 +0200

    only use segmentation, transcription, glossing

--- workflow/views.py
+++ workflow/views.py
@@ -7,9 +7,14 @@ def workflow_view(request):
     recordings = Recording.objects.all()
 
     for recording in recordings:
-        seg = Task.objects.filter(recording__name=recording.name)
-        segmentation = seg.filter(name='S').get()
+        tasks = Task.objects.filter(recording__name=recording.name)
+        segmentation = tasks.filter(name=Task.SEGMENTATION).get()
+        transcription = tasks.filter(name=Task.TRANSCRIPTION).get()
+        glossing = tasks.filter(name=Task.GLOSSING).get()
+
         recording.segmentation = segmentation.get_status_display()
+        recording.transcription = transcription.get_status_display()
+        recording.glossing = glossing.get_status_display()
 
     context = {
         'recordings': recordings
@@ -23,9 +28,7 @@ def rec_create_view(request):
         rec = form.save()
         Task.objects.create(recording=rec, name=Task.SEGMENTATION)
         Task.objects.create(recording=rec, name=Task.TRANSCRIPTION)
-        Task.objects.create(recording=rec, name=Task.TRANSCRIPTION_CHECK)
         Task.objects.create(recording=rec, name=Task.GLOSSING)
-        Task.objects.create(recording=rec, name=Task.GLOSSING_CHECK)
 
         return redirect('workflow:workflow')
 
