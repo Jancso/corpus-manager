df8a8e6d2bb1eb335df5130d8ed8241d6d7eae16
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-12 12:37:18 +0200

    Change layout of user profile update

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,24 @@ from .models import UserProfile
 from django.contrib.auth.models import User
 
 
-class UserProfileForm(ModelForm):
+class BootstrapForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            if visible.field.widget.input_type == 'file':
+                visible.field.widget.attrs['class'] = 'form-control-file btn btn-secondary'
+            else:
+                visible.field.widget.attrs['class'] = 'form-control'
+
+
+class UserProfileForm(BootstrapForm):
+
     class Meta:
         model = UserProfile
         fields = ['image', 'job_function']
 
 
-class UserForm(ModelForm):
+class UserForm(BootstrapForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
