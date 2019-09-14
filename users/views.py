fc97dee728992b4c2cef3f17ee1c3486a9756c24
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-14 10:26:40 +0200

    Only allow update of user profile for current user

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.views import View
 from django.views.generic.detail import DetailView
 from .forms import UserForm, UserProfileForm
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import User
 
 
@@ -24,11 +24,14 @@ class UserListView(LoginRequiredMixin, View):
         return render(request, self.template_name, context)
 
 
-class UserUpdateView(LoginRequiredMixin, View):
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, View):
 
     template_name = 'users/user_update.html'
     user_form = UserForm
     user_profile_form = UserProfileForm
+
+    def test_func(self):
+        return self.request.user.pk == self.kwargs.get('pk')
 
     def get_user(self, pk):
         user = None
