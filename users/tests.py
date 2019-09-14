6f1c6127324032447746a9418b1f0a2fa77ac351
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-14 10:55:13 +0200

    Some further tests for users

--- users/tests.py
+++ users/tests.py
@@ -1,12 +1,31 @@
-import unittest
+from django.test import TestCase
+from django.urls import reverse
 from django.test import Client
+from users.models import User, UserProfile
 
 
-class SimpleTest(unittest.TestCase):
+class UserTest(TestCase):
+
     def setUp(self):
         self.client = Client()
+        self.password = 'this_is_secret'
+        self.user = User.objects.create_user(
+            'test', 'test@test.ch', self.password)
+        UserProfile.objects.create(user=self.user)
 
-    def test_GET_users(self):
-        response = self.client.get('/users/')
+    def test_GET_user_list(self):
+        self.client.login(username=self.user.username, password=self.password)
+        response = self.client.get(reverse('users:user-list'))
         self.assertEqual(response.status_code, 200)
 
+    def test_GET_user_detail(self):
+        self.client.login(username=self.user.username, password=self.password)
+        response = self.client.get(reverse('users:user-detail',
+                                           args=[self.user.pk]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_GET_user_update(self):
+        self.client.login(username=self.user.username, password=self.password)
+        response = self.client.get(reverse('users:user-update',
+                                           args=[self.user.pk]))
+        self.assertEqual(response.status_code, 200)
