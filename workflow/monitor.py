bc4d59d9f90bf79fe86b11282fc502a4edf8be47
Anna Jancso <anna.jancso0808@gmail.com>
2019-09-14 20:53:25 +0200

    Add other recording parameters

--- workflow/monitor.py
+++ workflow/monitor.py
@@ -1,6 +1,7 @@
 import csv
 from io import StringIO
 from .models import Recording, Task
+from datetime import timedelta
 
 FIELDNAMES = [
     'recording name',
@@ -33,14 +34,53 @@ FIELDNAMES = [
     'notes'
 ]
 
+quality_names = {v: k for k, v in Recording.QUALITY_CHOICES}
+quality_names[''] = Recording.QUALITY_NA
+quality_names['little'] = Recording.QUALITY_LOW
+
+child_speech = {v: k for k, v in Recording.CHILD_SPEECH_CHOICES}
+child_speech[''] = Recording.CHILD_SPEECH_NA
+child_speech['low'] = Recording.CHILD_SPEECH_LITTLE
+child_speech['meidum'] = Recording.CHILD_SPEECH_MEDIUM
+
+directedness = {v: k for k, v in Recording.DIRECTEDNESS_CHOICES}
+directedness[''] = Recording.DIRECTEDNESS_NA
+
+dene = {v: k for k, v in Recording.DENE_SPEECH_CHOICES}
+dene[''] = Recording.DENE_SPEECH_NA
+dene['much '] = Recording.DENE_SPEECH_HIGH
+
+
+def to_timedelta(duration):
+    if duration:
+        if duration == '0':
+            return None
+        hours, minutes, seconds = duration.split(':')
+        timedelta(hours=int(hours), minutes=int(minutes), seconds=int(seconds))
+    else:
+        return None
+
+
 def import_(file):
     file = file.read().decode()
 
-    csv_data = csv.DictReader(StringIO(file), fieldnames=FIELDNAMES)
-    for row in csv_data:
-        print(row['recording name'])
+    reader = csv.DictReader(StringIO(file), fieldnames=FIELDNAMES)
+
+    next(reader)
+
+    for row in reader:
+
         rec, _ = Recording.objects.update_or_create(
-            name=row['recording name']
+            name=row['recording name'],
+            defaults={
+                'quality': quality_names[row['quality']],
+                'child_speech': child_speech[row['child speech']],
+                'directedness': directedness[row['directedness']],
+                'dene_speech': dene[row['Dene']],
+                'audio': row['audio'] if row['audio'] else None,
+                'length': to_timedelta(row['length']),
+                'notes': row['notes'] if row['notes'] else None
+            }
         )
 
         for task_name_i, task_name_h in Task.NAME_CHOICES:
