d2d159c820f526ab7c75709f03b3189cc16fcc01
Anna Jancso <anna.t.jancso@gmail.com>
2020-03-07 15:14:37 +0100

    Add some tests for recordings

---
+++ metadata/tests/test_recording.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.test import Client
+
+from metadata.models import Recording
+from workflow.models import Task
+
+
+class RecordingTest(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='test')
+        user.set_password('test')
+        user.save()
+        self.c = Client()
+        self.c.login(username='test', password='test')
+
+        rec = Recording.objects.create(name='deslas-AAA-2000-01-01')
+        Task.objects.create(recording=rec, name=Task.SEGMENTATION)
+        Task.objects.create(recording=rec, name=Task.TRANSCRIPTION)
+        Task.objects.create(recording=rec, name=Task.GLOSSING)
+
+    def test_recording_create(self):
+        response = self.c.get('/metadata/recordings/create/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_recording_update(self):
+        response = self.c.get('/metadata/recordings/1/update/')
+        self.assertEqual(response.status_code, 200)
--- metadata/urls.py
+++ metadata/urls.py
@@ -6,7 +6,7 @@ from metadata.views import rec_create_view, rec_delete_view, \
 app_name = 'metadata'
 
 urlpatterns = [
-    path('rec/create/', rec_create_view, name='rec-create'),
+    path('recordings/create/', rec_create_view, name='rec-create'),
     path('recordings/<int:pk>/delete/', rec_delete_view, name='rec-delete'),
     path('recordings/<int:pk>/update/', RecordingUpdateView.as_view(), name='rec-update'),
 ]
