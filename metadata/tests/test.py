5912ea78fcf4da7c3b8ddd90b9000521eb89b7b3
Anna Jancso <anna.t.jancso@gmail.com>
2020-03-08 22:31:42 +0100

    Add metadata test

--- metadata/tests/test.py
+++ metadata/tests/test.py
@@ -5,13 +5,19 @@ from django.test import Client
 
 class MetadataTest(TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         user = User.objects.create(username='test')
         user.set_password('test')
         user.save()
-        self.c = Client()
-        self.c.login(username='test', password='test')
+        cls.c = Client()
+        cls.c.login(username='test', password='test')
 
-    def test_recording_create(self):
+    def test_metdata_create(self):
+        response = self.c.get('/metadata/create/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_metdata(self):
         response = self.c.get('/metadata/')
         self.assertEqual(response.status_code, 200)
