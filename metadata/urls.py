cb479c4df4b1934c9619ddf31f4371eb6f9eb151
Anna Jancso <anna.t.jancso@gmail.com>
2020-04-13 12:07:18 +0200

    Add language update

--- metadata/urls.py
+++ metadata/urls.py
@@ -10,7 +10,7 @@ from metadata.views.file_views import file_list_view
 from metadata.views.participant_views import \
     participant_list_view, ParticipantCreateView, participant_detail_view, \
     ParticipantUpdateView, participant_delete_view
-from metadata.views.language_views import language_list_view
+from metadata.views.language_views import language_list_view, language_list_update_view
 
 app_name = 'metadata'
 
@@ -20,6 +20,7 @@ urlpatterns = [
     path('create/', metadata_create_view, name='metadata-create'),
 
     path('languages/', language_list_view, name='language-list'),
+    path('languages/update/', language_list_update_view, name='language-list-update'),
 
     path('sessions/', session_list_view, name='session-list'),
     path('sessions/<int:pk>/', session_detail_view, name='session-detail'),
--- metadata/views/language_views.py
+++ metadata/views/language_views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from metadata.models import Language
+
+import requests
+import csv
 
 
 @login_required
@@ -10,3 +13,31 @@ def language_list_view(request):
     context = {'languages': languages}
     return render(request,
                   'metadata/language/language_list.html', context)
+
+
+def update_languages():
+    Language.objects.all().delete()
+
+    url = 'https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3.tab'
+
+    with requests.Session() as s:
+        download = s.get(url)
+
+        decoded_content = download.content.decode('utf-8')
+
+        languages = []
+
+        reader = csv.reader(decoded_content.splitlines(), delimiter='\t')
+        for row in reader:
+            iso = row[0]
+            name = row[6]
+
+            languages.append(Language(iso_code=iso, name=name))
+
+        Language.objects.bulk_create(languages)
+
+
+@login_required
+def language_list_update_view(_):
+    update_languages()
+    return redirect('metadata:language-list')
