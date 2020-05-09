56090a5f67ff38a94dc547f91557bc321b75fd76
Anna Jancso <anna.t.jancso@gmail.com>
2020-05-09 13:55:42 +0200

    Add token creation card

--- backup/urls.py
+++ backup/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from backup.views import backup_view
+from backup.views import backup_view, TokenCreateView
 
 app_name = 'backup'
 
 urlpatterns = [
-    path('', backup_view, name='backup-view')
+    path('', backup_view, name='backup-view'),
+    path('token/create/', TokenCreateView.as_view(), name='token-create-view'),
 ]
--- backup/views.py
+++ backup/views.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+
+from backup.forms import TokenForm
 
 
 @login_required
 def backup_view(request):
     return render(request, 'backup/backup_overview.html', {})
+
+
+class TokenCreateView(LoginRequiredMixin, View):
+    template = 'backup/token_create.html'
+
+    def get(self, request):
+        form = TokenForm()
+        context = {'form': form}
+        return render(request, self.template, context)
+
+    def post(self, request):
+        form = TokenForm(request.POST)
+        if form.is_valid():
+            return redirect('backup:backup-view')
+
+        context = {'form': form}
+        return render(request, self.template, context)
