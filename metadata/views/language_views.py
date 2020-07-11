b998b8642132901509e9fd2fedf24dc77db973d0
Anna Jancso <anna.t.jancso@gmail.com>
2020-07-11 23:34:08 +0200

    Prettify language list

--- metadata/views/language_views.py
+++ metadata/views/language_views.py
@@ -5,9 +5,14 @@ from metadata.imports.import_languages import import_languages
 from metadata.models import Language
 
 
+def grouped(objs, n):
+    for i in range(0, len(objs), n):
+        yield objs[i:i + n]
+
+
 @login_required
 def language_list_view(request):
-    languages = Language.objects.order_by('name')
+    languages = grouped(Language.objects.order_by('name'), 3)
     context = {'languages': languages}
     return render(request,
                   'metadata/language/language_list.html', context)
