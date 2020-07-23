6f97d8fc0c9a21f534012903656bcb566642a8ef
Anna Jancso <anna.t.jancso@gmail.com>
2020-07-23 12:27:10 +0200

    Refactor urls

--- metadata/urls.py
+++ metadata/urls.py
@@ -1,65 +1,126 @@
 from django.urls import path
 
-from metadata.views.rec_views import rec_delete_view, \
-    RecordingUpdateView, rec_list_view, rec_create_view, rec_detail_view
-from metadata.views.views import metadata_view, MetadataImportView
-from metadata.views.role_views import role_import_view, role_list_view, role_create_view, role_delete_view, role_update_view
-from metadata.views.session_views import \
-    session_list_view, session_detail_view, SessionUpdateView, \
-    session_delete_view, session_participants_create_view, \
-    session_participant_delete_view, session_csv_export_view, \
-    SessionParticipantUpdateView
-from metadata.views.file_views import file_list_view
-from metadata.views.participant_views import \
-    participant_list_view, ParticipantCreateView, participant_detail_view, \
-    ParticipantUpdateView, participant_delete_view, ParticipantLangInfoCreateView, participant_language_delete_view, ParticipantLangUpdateView, participant_csv_export_view
-from metadata.views.language_views import language_list_view, language_import_view, language_create_view, language_update_view, language_delete_view
+from metadata.views import general_views
+from metadata.views import role_views
+from metadata.views import session_views
+from metadata.views import file_views
+from metadata.views import participant_views
+from metadata.views import language_views
+from metadata.views import rec_views
 
 app_name = 'metadata'
 
 urlpatterns = [
-    path('', metadata_view, name='metadata-view'),
+    path('',
+         general_views.metadata_view,
+         name='metadata-view'),
 
-    path('languages/', language_list_view, name='language-list'),
-    path('languages/create/', language_create_view, name='language-create'),
-    path('languages/<int:pk>/update/', language_update_view, name='language-update'),
-    path('languages/<int:pk>/delete/', language_delete_view, name='language-delete'),
-    path('languages/import/', language_import_view, name='language-import'),
+    path('languages/',
+         language_views.language_list_view,
+         name='language-list'),
+    path('languages/create/',
+         language_views.language_create_view,
+         name='language-create'),
+    path('languages/<int:pk>/update/',
+         language_views.language_update_view,
+         name='language-update'),
+    path('languages/<int:pk>/delete/',
+         language_views.language_delete_view,
+         name='language-delete'),
+    path('languages/import/',
+         language_views.language_import_view,
+         name='language-import'),
 
-    path('roles/', role_list_view, name='role-list'),
-    path('roles/create/', role_create_view, name='role-create'),
-    path('roles/<int:pk>/update/', role_update_view, name='role-update'),
-    path('roles/<int:pk>/delete/', role_delete_view, name='role-delete'),
-    path('roles/import/', role_import_view, name='role-import'),
+    path('roles/',
+         role_views.role_list_view,
+         name='role-list'),
+    path('roles/create/',
+         role_views.role_create_view,
+         name='role-create'),
+    path('roles/<int:pk>/update/',
+         role_views.role_update_view,
+         name='role-update'),
+    path('roles/<int:pk>/delete/',
+         role_views.role_delete_view,
+         name='role-delete'),
+    path('roles/import/',
+         role_views.role_import_view,
+         name='role-import'),
 
-    path('sessions/', session_list_view, name='session-list'),
-    path('sessions/<int:pk>/', session_detail_view, name='session-detail'),
-    path('sessions/<int:pk>/update/', SessionUpdateView.as_view(), name='session-update'),
-    path('sessions/<int:pk>/delete/', session_delete_view, name='session-delete'),
-    path('sessions/<int:pk>/participants/create/', session_participants_create_view, name='session-participants-create'),
-    path('sessions/<int:spk>/participants/<int:ppk>/delete/', session_participant_delete_view, name='session-participant-delete'),
+    path('sessions/',
+         session_views.session_list_view,
+         name='session-list'),
+    path('sessions/<int:pk>/',
+         session_views.session_detail_view,
+         name='session-detail'),
+    path('sessions/<int:pk>/update/',
+         session_views.SessionUpdateView.as_view(),
+         name='session-update'),
+    path('sessions/<int:pk>/delete/',
+         session_views.session_delete_view,
+         name='session-delete'),
+    path('sessions/<int:pk>/participants/create/',
+         session_views.session_participants_create_view,
+         name='session-participants-create'),
+    path('sessions/<int:spk>/participants/<int:ppk>/delete/',
+         session_views.session_participant_delete_view,
+         name='session-participant-delete'),
     path('sessions/<int:spk>/participants/<int:ppk>/update/',
-         SessionParticipantUpdateView.as_view(),
+         session_views.SessionParticipantUpdateView.as_view(),
          name='session-participant-update'),
-    path('sessions/export/csv/', session_csv_export_view, name='session-csv-export'),
+    path('sessions/export/csv/',
+         session_views.session_csv_export_view,
+         name='session-csv-export'),
 
-    path('participants/', participant_list_view, name='participant-list'),
-    path('participants/create/', ParticipantCreateView.as_view(), name='participant-create'),
-    path('participants/<int:pk>/', participant_detail_view, name='participant-detail'),
-    path('participants/<int:pk>/update/', ParticipantUpdateView.as_view(), name='participant-update'),
-    path('participants/<int:pk>/delete/', participant_delete_view, name='participant-delete'),
-    path('participants/<int:pk>/languages/create/', ParticipantLangInfoCreateView.as_view(), name='participant-lang-create'),
-    path('participants/<int:ppk>/languages/<int:lpk>/delete/', participant_language_delete_view , name='participant-lang-delete'),
-    path('participants/<int:ppk>/languages/<int:lpk>/update/', ParticipantLangUpdateView.as_view(), name='participant-lang-update'),
-    path('participants/export/csv/', participant_csv_export_view, name='participant-csv-export'),
+    path('participants/',
+         participant_views.participant_list_view,
+         name='participant-list'),
+    path('participants/create/',
+         participant_views.ParticipantCreateView.as_view(),
+         name='participant-create'),
+    path('participants/<int:pk>/',
+         participant_views.participant_detail_view,
+         name='participant-detail'),
+    path('participants/<int:pk>/update/',
+         participant_views.ParticipantUpdateView.as_view(),
+         name='participant-update'),
+    path('participants/<int:pk>/delete/',
+         participant_views.participant_delete_view,
+         name='participant-delete'),
+    path('participants/<int:pk>/languages/create/',
+         participant_views.ParticipantLangInfoCreateView.as_view(),
+         name='participant-lang-create'),
+    path('participants/<int:ppk>/languages/<int:lpk>/delete/',
+         participant_views.participant_language_delete_view,
+         name='participant-lang-delete'),
+    path('participants/<int:ppk>/languages/<int:lpk>/update/',
+         participant_views.ParticipantLangUpdateView.as_view(),
+         name='participant-lang-update'),
+    path('participants/export/csv/',
+         participant_views.participant_csv_export_view,
+         name='participant-csv-export'),
 
-    path('recordings/', rec_list_view, name='rec-list'),
-    path('recordings/<int:pk>/', rec_detail_view, name='rec-detail'),
-    path('recordings/<int:pk>/delete/', rec_delete_view, name='rec-delete'),
-    path('recordings/<int:pk>/update/', RecordingUpdateView.as_view(), name='rec-update'),
-    path('recordings/create/', rec_create_view, name='rec-create'),
+    path('recordings/',
+         rec_views.rec_list_view,
+         name='rec-list'),
+    path('recordings/<int:pk>/',
+         rec_views.rec_detail_view,
+         name='rec-detail'),
+    path('recordings/<int:pk>/delete/',
+         rec_views.rec_delete_view,
+         name='rec-delete'),
+    path('recordings/<int:pk>/update/',
+         rec_views.RecordingUpdateView.as_view(),
+         name='rec-update'),
+    path('recordings/create/',
+         rec_views.rec_create_view,
+         name='rec-create'),
 
-    path('files/', file_list_view, name='file-list'),
+    path('files/',
+         file_views.file_list_view,
+         name='file-list'),
 
-    path('import/', MetadataImportView.as_view(), name='metadata-import')
+    path('import/',
+         general_views.MetadataImportView.as_view(),
+         name='metadata-import')
 ]
