b46a2b96617d894b4d38bd0152e11616a1e302e5
Anna Jancso <anna.t.jancso@gmail.com>
2021-02-16 16:01:44 +0100

    Add unique constraint

--- metadata/models.py
+++ metadata/models.py
@@ -403,7 +403,7 @@ class SessionParticipantRole(models.Model):
 
 
 class File(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     recording = models.ForeignKey('metadata.Recording', on_delete=models.CASCADE)
 
